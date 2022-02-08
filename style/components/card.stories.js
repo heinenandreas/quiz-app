2c4ccdd786b84fbac18c124e45a530c3229c109e
Andreas Heinen <andreas.heinen@posteo.de>
2022-02-08 17:49:13 +0100

    configure components and sotrybook

---
+++ style/components/card.stories.js
@@ -0,0 +1,36 @@
+export default {
+  title: "Components/Card",
+};
+
+export const Default = () => /*html*/ `
+<article class="card-layout">
+
+    <div class="card-head">
+        <h1 class= "question-headline">Question:</h1>
+    </div>
+
+    <p class="question-text">
+        Wie lang kann so eine Frage schon sein?
+    </p>
+
+    <div class="button-show-answer">
+        <p class="show-answer">show answer</p>
+    </div>
+
+    <div class="categories">
+        <div class="cat-active">
+            <p class="cat-name">HTML</p>    
+        </div>
+        <div class="cat-inactive">
+            <p class="cat-name">CSS</p> 
+        </div>
+        <div class="cat-inactive">
+            <p class="cat-name">Flexbox</p> 
+        </div>
+        <div class="cat-inactive">
+            <p class="cat-name">Grid</p> 
+        </div>
+    </div>
+
+</article>
+`;
