954a71cad4e4b955c45a3c09692cf7759fd75e8b
Andreas Heinen <andreas.heinen@posteo.de>
2022-02-09 10:00:07 +0100

    modify component footer

--- style/components/footer.stories.js
+++ style/components/footer.stories.js
@@ -4,12 +4,12 @@ export default {
 
 export const Default = () => /*html*/ `
 <footer class="footer-layout">
-<nav>
-  <ul class="navigation">
-    <li><a href="/index.html">Home</a></li>
-    <li><a href="/bookmarks.html">Bookmark</a></li>
-    <li><a href="/create.html">Create</a></li>
-    <li><a href="/profile.html">Profile</a></li>
+<nav class = "navigation">
+  <ul class="navigation-ul">
+    <li class="navigation-li"><a href="/index.html">Home</a></li>
+    <li class="navigation-li"><a href="/bookmarks.html">Bookmark</a></li>
+    <li class="navigation-li"><a href="/create.html">Create</a></li>
+    <li class="navigation-li"><a href="/profile.html">Profile</a></li>
   </ul>
 </nav>
 </footer>
